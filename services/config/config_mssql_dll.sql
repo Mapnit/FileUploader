361acd18af5004d47b6530e3a123049cae76fd00
jaredlang <jaredlang76@gmail.com>
2019-03-27 12:16:23 -0500

    changed date to datetime to capture the timestamp
    also moved the drop statements to the beginning

--- services/config/config_mssql_dll.sql
+++ services/config/config_mssql_dll.sql
@@ -1,56 +1,53 @@
-DROP TABLE [CHEN_FILE_UPLOADS_SHARED]; 
 
-CREATE TABLE [CHEN_FILE_UPLOADS_SHARED] (	
+drop table CHEN_FILE_STYLES; 
+drop table CHEN_FILE_UPLOADS_ARCHIVED;
+drop table CHEN_FILE_UPLOADS_SHARED;
+drop table CHEN_FILE_UPLOADS; 
+
+CREATE TABLE CHEN_FILE_STYLES (
 	OWNER char(30),
-	SRC_FILE_PATH NVARCHAR (200),
-	SHARED_USER char(30) NOT NULL,
-	SHARED_DATE  DATE NOT NULL,  
-	primary key (owner, src_file_path, shared_user), 
-	foreign key (owner, src_file_path) references [CHEN_FILE_UPLOADS](owner, src_file_path) on delete cascade
-);
-
-DROP TABLE [CHEN_FILE_UPLOADS]; 
-
-CREATE TABLE [CHEN_FILE_UPLOADS] (
-    [OWNER]            CHAR (30)      NOT NULL,
-    [SRC_FILE_PATH]    NVARCHAR (200) NOT NULL,
-    [CACHE_FILE_PATH]  NTEXT		  NULL,
-    [DATA_NAME]        CHAR (100)     NULL,
-    [DATA_SIZE]        INT            NULL,
-    [LAST_MODIFIED]    DATE           NOT NULL,
-    [LAST_UPLOADED]    DATE           NOT NULL,
-    [LAST_ACCESSED]    DATE           NOT NULL,
-    [UPLOAD_STATUS]    CHAR (25)      NOT NULL,
-    [TOTAL_ROW_COUNT]  INT            NOT NULL,
-    [CACHED_ROW_COUNT] INT            NOT NULL,
-    PRIMARY KEY CLUSTERED ([OWNER] ASC, [SRC_FILE_PATH] ASC)
-);
-
-DROP TABLE [CHEN_FILE_UPLOADS_ARCHIVED];
-
-CREATE TABLE [CHEN_FILE_UPLOADS_ARCHIVED] (
-    [OWNER]            CHAR (30)      NOT NULL,
-    [SRC_FILE_PATH]    NVARCHAR (200) NOT NULL,
-    [CACHE_FILE_PATH]  NTEXT		  NULL,
-    [DATA_NAME]        CHAR (100)     NULL,
-    [DATA_SIZE]        INT            NULL,
-    [LAST_MODIFIED]    DATE           NOT NULL,
-    [LAST_UPLOADED]    DATE           NOT NULL,
-    [LAST_ACCESSED]    DATE           NOT NULL,
-    [UPLOAD_STATUS]    CHAR (25)      NOT NULL,
-    [TOTAL_ROW_COUNT]  INT            NOT NULL,
-    [CACHED_ROW_COUNT] INT            NOT NULL,
-    [ARCHIVED]         DATE           NOT NULL,
-    PRIMARY KEY CLUSTERED ([OWNER] ASC, [SRC_FILE_PATH] ASC, [ARCHIVED] ASC)
-);
-
-
-DROP TABLE [CHEN_FILE_STYLES]; 
-
-CREATE TABLE [CHEN_FILE_STYLES] (
-	OWNER char(30),
-	SRC_FILE_PATH NVARCHAR (200),
-	DRAWING_INFO NTEXT,
+	SRC_FILE_PATH nvarchar(200),
+	DRAWING_INFO TEXT,
 	primary key (owner, src_file_path)
 ); 
 
+CREATE TABLE CHEN_FILE_UPLOADS (
+	OWNER char(30),
+	SRC_FILE_PATH nvarchar(200),
+	CACHE_FILE_PATH ntext,
+	DATA_NAME char(100),
+	DATA_SIZE integer,
+	LAST_MODIFIED datetime NOT NULL,
+	LAST_UPLOADED datetime NOT NULL,
+	LAST_ACCESSED datetime NOT NULL, 
+	UPLOAD_STATUS char(25) NOT NULL,
+	TOTAL_ROW_COUNT integer NOT NULL,
+	CACHED_ROW_COUNT integer NOT NULL, 
+	--EMAIL char(100)
+	primary key (owner, src_file_path)
+);
+  
+CREATE TABLE CHEN_FILE_UPLOADS_ARCHIVED (	
+	OWNER char(30),
+	SRC_FILE_PATH nvarchar(200),
+	CACHE_FILE_PATH ntext,
+	DATA_NAME char(100),
+	DATA_SIZE integer,
+	LAST_MODIFIED datetime NOT NULL,
+	LAST_UPLOADED datetime NOT NULL,
+	LAST_ACCESSED datetime NOT NULL, 
+	UPLOAD_STATUS char(25) NOT NULL,
+	TOTAL_ROW_COUNT integer NOT NULL,
+	CACHED_ROW_COUNT integer NOT NULL, 	
+	ARCHIVED  datetime,  
+	primary key (owner, src_file_path, archived)
+);
+
+CREATE TABLE CHEN_FILE_UPLOADS_SHARED (	
+	OWNER char(30),
+	SRC_FILE_PATH nvarchar(200),
+	SHARED_USER char(30) NOT NULL,
+	SHARED_DATE  datetime NOT NULL,  
+	primary key (owner, src_file_path, shared_user), 
+	foreign key (owner, src_file_path) references CHEN_FILE_UPLOADS(owner, src_file_path) on delete cascade
+);
