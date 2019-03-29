e7b5205390e8b748531126f6975c1d2d0acf7826
jaredlang <jaredlang76@gmail.com>
2019-03-29 11:12:11 -0500

    changed data types from char to varchar
    char(25) pads the values with blanks to make 25 chars.

--- services/config/config_mssql_dll.sql
+++ services/config/config_mssql_dll.sql
@@ -5,38 +5,38 @@ drop table CHEN_FILE_UPLOADS_SHARED;
 drop table CHEN_FILE_UPLOADS; 
 
 CREATE TABLE CHEN_FILE_STYLES (
-	OWNER char(30),
+	OWNER nvarchar(30),
 	SRC_FILE_PATH nvarchar(200),
 	DRAWING_INFO TEXT,
 	primary key (owner, src_file_path)
 ); 
 
 CREATE TABLE CHEN_FILE_UPLOADS (
-	OWNER char(30),
+	OWNER nvarchar(30),
 	SRC_FILE_PATH nvarchar(200),
 	CACHE_FILE_PATH ntext,
-	DATA_NAME char(100),
+	DATA_NAME nvarchar(100),
 	DATA_SIZE integer,
 	LAST_MODIFIED datetime NOT NULL,
 	LAST_UPLOADED datetime NOT NULL,
 	LAST_ACCESSED datetime NOT NULL, 
-	UPLOAD_STATUS char(25) NOT NULL,
+	UPLOAD_STATUS nvarchar(25) NOT NULL,
 	TOTAL_ROW_COUNT integer NOT NULL,
 	CACHED_ROW_COUNT integer NOT NULL, 
-	--EMAIL char(100)
+	--EMAIL nvarchar(100)
 	primary key (owner, src_file_path)
 );
   
 CREATE TABLE CHEN_FILE_UPLOADS_ARCHIVED (	
-	OWNER char(30),
+	OWNER nvarchar(30),
 	SRC_FILE_PATH nvarchar(200),
 	CACHE_FILE_PATH ntext,
-	DATA_NAME char(100),
+	DATA_NAME nvarchar(100),
 	DATA_SIZE integer,
 	LAST_MODIFIED datetime NOT NULL,
 	LAST_UPLOADED datetime NOT NULL,
 	LAST_ACCESSED datetime NOT NULL, 
-	UPLOAD_STATUS char(25) NOT NULL,
+	UPLOAD_STATUS nvarchar(25) NOT NULL,
 	TOTAL_ROW_COUNT integer NOT NULL,
 	CACHED_ROW_COUNT integer NOT NULL, 	
 	ARCHIVED  datetime,  
@@ -44,9 +44,9 @@ CREATE TABLE CHEN_FILE_UPLOADS_ARCHIVED (
 );
 
 CREATE TABLE CHEN_FILE_UPLOADS_SHARED (	
-	OWNER char(30),
+	OWNER nvarchar(30),
 	SRC_FILE_PATH nvarchar(200),
-	SHARED_USER char(30) NOT NULL,
+	SHARED_USER nvarchar(30) NOT NULL,
 	SHARED_DATE  datetime NOT NULL,  
 	primary key (owner, src_file_path, shared_user), 
 	foreign key (owner, src_file_path) references CHEN_FILE_UPLOADS(owner, src_file_path) on delete cascade
